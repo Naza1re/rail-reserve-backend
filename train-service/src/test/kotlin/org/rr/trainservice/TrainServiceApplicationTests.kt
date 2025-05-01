b0192baa80e7f8f4f3b38cca87cdf4245ee1de58
Бачище Назар Сергеевич <mcarim@mail.ru>
2025-05-01 22:13:23 +0300

    init commit for train-service

---
+++ train-service/src/test/kotlin/org/rr/trainservice/TrainServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.rr.trainservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TrainServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
