91c892f7d103e942e078062794b05fa287af1c25
Бачище Назар Сергеевич <mcarim@mail.ru>
2025-05-05 21:40:10 +0300

    ticker-service: init commit

---
+++ ticket-service/src/test/kotlin/org/rr/ticketservice/TicketServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.rr.ticketservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TicketServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
